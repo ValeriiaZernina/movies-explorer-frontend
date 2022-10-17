2e3f3f56d345b66812dd3c68abb81ddd022783ff
Valeria Zernina <darovanie@mail.ru>
2022-10-18 00:14:50 +0500

    fonts

--- movies_explorer_frontend/src/components/App/App.js
+++ movies_explorer_frontend/src/components/App/App.js
@@ -1,15 +1,32 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Redirect, Route } from "react-router-dom";
-import { route } from "express/lib/application";
+import { Route, Routes } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import Profile from "../Profile/Profile";
+import PageNotFound from "../PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div className="page">
-      <Routes>
-        <Route path="/"></Route>
-      </Routes>
+      <Header></Header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/movies" element={<Movies />}></Route>
+          <Route path="/saved-movies" element={<SavedMovies />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/signin" element={<Register />}></Route>
+          <Route path="/signup" element={<Login />}></Route>
+          <Route path="/*" element={<PageNotFound />}></Route>
+        </Routes>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
