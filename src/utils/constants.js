e65ee05d6cc14769fffdb6f99b4206600aab9caa
Valeria Zernina <darovanie@mail.ru>
2022-11-09 08:26:09 +0500

    const URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-const URL = "http://localhost:4000";
-// const URL = "https://api.zernina.nomoredomains.icu";
+//const URL = "http://localhost:4000";
+const URL = "https://api.zernina.nomoredomains.icu";
 const MOVIES_URL = "https://api.nomoreparties.co";
 const SHORT_DURATION = 40;
 
